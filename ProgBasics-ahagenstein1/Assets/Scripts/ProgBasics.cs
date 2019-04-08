3c108d3b7c4c6b3495f905f560b95a06437a3da6
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2019-04-08 15:12:16 +0200

    updated README.md

---
+++ ProgBasics-ahagenstein1/Assets/Scripts/ProgBasics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgBasics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("erste Tests mit Operatoren");
+        Debug.Log("Multiplikative Operatoren: * " + (2 * 4));
+        Debug.Log("Multiplikative Operatoren:  / " + (2 / 4));
+        Debug.Log("Multiplikative Operatoren:  % " + (2 % 4));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+
 # ProgBasics-ahagenstein
+Operatoren Test 
+Unity Version: 2018.2.14f1
+Visual Studio Version: 7.7.2.21
